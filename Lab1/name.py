c8f56a06ddd0df342a24d654b098fb8ff80af5df
Bibekbahadurkc <bibekbahadurkc@gmail.com>
2021-02-24 11:47:03 +0545

    Name

---
+++ Lab1/name.py
@@ -0,0 +1,8 @@
+#name and age
+name = input('Enter your name')
+age = input('Enter your age')
+print(f'My name is {name}.And my age {age}')
+print('my name is',name, 'and my age is',age)
+print("my name is {} and i am {} years old".format(name,age))
+print("my name is %s and i am %s years old" % (name, age))
+print('2 My name is ' + name +'And my age is ' +age)
