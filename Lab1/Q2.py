f0116cdfd394a343e64bbcf31850a9cac7f896d5
Bibekbahadurkc <bibekbahadurkc@gmail.com>
2021-02-24 11:26:53 +0545

    Area

--- Lab1/Q2.py
+++ Lab1/Q2.py
@@ -1,2 +1,5 @@
-#2. Write a program that reads the length of the base and the height of a right_angled triangle and prints the area. Every number is given on a separate
-print(line)
+#2. Write a program that reads the length of the base and the height of a right_angled triangle and prints the area. Every number is given on a separate print(line)
+length = int(input('Enter the length'))
+height = int(input('Enter the height'))
+area = 1/2 * length * height
+print(f'The area ={area}')
