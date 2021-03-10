02aa841acd615fb25b7515f469541c7d78296373
Bibekbahadurkc <bibekbahadurkc@gmail.com>
2021-03-10 11:54:26 +0545

    BMI

--- Lab1/Q6.py
+++ Lab1/Q6.py
@@ -4,4 +4,5 @@
 mass = float(input("enter the mass of person in kg: "))
 height = float(input("enter the height of a person in meter: "))
 BMI = (mass / (height * height))
+
 print(f"The BMI index of a person is {BMI}")
