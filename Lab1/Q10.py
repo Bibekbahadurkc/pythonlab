c48647e412bd733c6e7965009324a3b9dee02648
Bibekbahadurkc <bibekbahadurkc@gmail.com>
2021-03-10 11:51:22 +0545

    Convert seconds to day,hour,min,sec

--- Lab1/Q10.py
+++ Lab1/Q10.py
@@ -1,1 +1,10 @@
 #Write a Python program to convert seconds to day, hour, minutes and seconds.
+second = int(input("Enter the value of second: "))
+day = (((second//60)//60)//24)
+print(f"Total day for given seconds: {day}")
+
+hour = ((second//60)//60)
+print(f"Total hours for given seconds: {hour}")
+
+minutes = (second//60)
+print(f"Total minutes for given seconds: {minutes}")
