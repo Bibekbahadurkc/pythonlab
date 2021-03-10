0dfdbc58c8c6050a614e4fe90a596ce73fc31a82
Bibekbahadurkc <bibekbahadurkc@gmail.com>
2021-03-10 11:35:51 +0545

    Sum of positive integers

---
+++ Lab1/Q8.py
@@ -0,0 +1,2 @@
+#write a Python program which accepts the radius of a circle from the user and compute the area.(area of circle = PI * r^2)
+radius = int(input())
---
+++ Lab1/Q9.py
@@ -0,0 +1,9 @@
+#write a python program to find sum of the first n positive integers.
+#sum = (n*(n+1))/2
+#n = int(input("Input  a number:"))
+#sum = (n*(n+1)) / 2
+#print(sum)
+
+list = [1,2,3,4,5,6,7,8,9,10]
+total=sum(list)
+print(f"The sum of given positive integer is {total}")
